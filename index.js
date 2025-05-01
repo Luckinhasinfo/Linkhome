36745dcb2e4739bf1083fac4273bd314bd9676f4
Lucas <lao9@aluno.ifnmg.edu.br>
2025-05-01 18:09:56 -0300

    SYNC de 18:09 do 01/05

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();//pense nisso como o servidor
+app.use(express.json());//consegue enteder o arquivo JSON  com os bgl la
+const db = require('./db');//nao entendi muito
+const PORT = 3000; //coloquei a 3000 mas quem quiser muda
+
+app.get('/', (req, res) => {
+     res.send('Servidor funcionando com sucesso!');
+});//testa a porra do serv
+
+app.listen(PORT, () => {
+     console.log(`Servidor rodando na porta ${PORT}`);
+});//roda o serv
+   
+   
