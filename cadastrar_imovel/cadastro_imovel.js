776aaebb6a1390a845bf44d4c12b303a90457a5e
Lucas <lao9@aluno.ifnmg.edu.br>
2025-05-08 23:36:49 -0300

    nome das pastas

--- cadastrar_imovel/cadastro_imovel.js
+++
@@ -1,2 +0,0 @@
-let cadastro_imovel_cep = documento.getElementById('cadastro_imovel_cep').value;
-
---
+++ cadastrar_novo_imovel/cadastrar_novo_imovel.js
@@ -0,0 +1,5 @@
+let imovel_cep = documento.getElementById('imovel_cep').value;
+let imovel_bairro = documento.getElementById('imovel_bairro').value;
+let imovel_numero = documento.getElementById('imovel_numero').value;
+let imovel_logradouro = documento.getElementById('imovel_logradouro').value;
+
