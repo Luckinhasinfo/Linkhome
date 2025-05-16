e1d5b0b55c874ef807541615147743a68e229ba7
Lucas <lao9@aluno.ifnmg.edu.br>
2025-05-16 15:55:29 -0300

    Co-authored-by: Leonardo-Git <Leonardo-Git@users.noreply.github.com> Co-authored-by: vitote1 <vitote1@users.noreply.github.com> Co-authored-by: Aranha <Skollt007@users.noreply.github.com>

---
+++ cadastro_inicial/cadastro_inicial.js
@@ -0,0 +1,4 @@
+let floatingEmail = document.getElementById('floatingEmail').value;
+let floatingCPF = document.getElementById('floatingCPF').value;
+let floatingPassword = document.getElementById('floatingPassword').value;
+let floatingTelefone = document.getElementById('floatingTelefone').value;
