4d21bde67a9d665610a64ee8576ec8ec01261ee5
jesus <162645683+Jess0git@users.noreply.github.com>
2025-05-31 09:50:45 -0300

    Update cadastro_inicial.js

--- cadastro_inicial/cadastro_inicial.js
+++ cadastro_inicial/cadastro_inicial.js
@@ -1,9 +1,55 @@
-let floatingEmail = document.getElementById('floatingEmail').value;
-let floatingCPF = document.getElementById('floatingCPF').value;
-let floatingPassword = document.getElementById('floatingPassword').value;
-let floatingTelefone = document.getElementById('floatingTelefone').value;
-let floatingData = document.getElementById('floatingData').value;
-let floatingConfirmarSenha = document.getElementById('floatingConfirmarSenha').value;
+let botCadastro = document.getElementById('botCadastro');
+botCadastro.addEventListener('click', cadastrarUsuario);
 
-let botao = document.getElementById('botCadastro');
-botao.addEventListener('click', () => {window.location.href="../reservar_imovel/reservar_imovel.html"});
+
+function cadastrarUsuario() {
+    let email = document.getElementById('floatingEmail').value;
+    let cpf = document.getElementById('floatingCPF').value;
+    let senha = document.getElementById('floatingPassword').value;
+    let telefone = document.getElementById('floatingTelefone').value;
+    let dataNascimento = document.getElementById('floatingData').value;
+    let confirmarSenha = document.getElementById('floatingConfirmarSenha').value;
+
+    if (senha !== confirmarSenha) {
+        alert("As senhas não coincidem.");
+        return;
+    }
+
+
+
+    if ( !email || !cpf || !senha || !telefone || !dataNascimento) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+    enviarCadastro(email, cpf, senha, telefone, dataNascimento);
+  
+}
+
+
+
+async function enviarCadastro(email, cpf, senha, telefone, dataNascimento) {
+  try {
+    const resposta = await fetch('http://localhost:3000/usuarios', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+       email: email,
+        cpf:cpf,
+        senha:senha,
+        telefone: telefone,
+        data_nascimento: dataNascimento
+      })
+    });
+
+    const resultado = await resposta.json();
+
+    if (resposta.ok) {
+      alert('Usuário cadastrado com sucesso!');
+    } else {
+      alert('Erro: ' + resultado.erro);
+    }
+  } catch (error) {
+    alert('Erro inesperado: ' + error.message);
+  }
+  
+}
