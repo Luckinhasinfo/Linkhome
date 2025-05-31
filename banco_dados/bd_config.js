07e0b0cfa49958fe63a38dbc17ea861e002bc2dd
jesus <162645683+Jess0git@users.noreply.github.com>
2025-05-31 09:49:39 -0300

    Update bd_config.js

--- banco_dados/bd_config.js
+++ banco_dados/bd_config.js
@@ -6,7 +6,8 @@ const conexao = mysql.createConnection({
   host: 'localhost',     // Endereço do servidor do banco de dados
   user: 'root',          // Nome do usuário do banco (padrão: root)
   password: '',          // Senha do banco (vazia no XAMPP/WAMP padrão)
-  database: 'crud_usuarios' // Nome do banco que criamos
+  database: 'crud_node', // Nome do banco que criamos
+  port: 3306           // Porta do MySQL (padrão: 3306)
 });
 
 // 3. Conecta ao banco e mostra erro ou sucesso
