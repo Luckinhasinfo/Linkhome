956e2a804b0de0fd35a7fb3bc90213b1c5ba9649
jesus <162645683+Jess0git@users.noreply.github.com>
2025-05-31 09:48:42 -0300

    Update index.js
    agr funciona

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();//pense nisso como o servidor
-app.use(express.json());//consegue enteder o arquivo JSON  com os bgl la
-const db = require('banco_dados/bd_config');//e nosso arquivo de conect com bd
+
+app.use(cors());
+app.use(express.json());
+
+const usuariosRouter = require('./banco_dados/usuarios'); // Importa o arquivo de rotas
+
+app.use('/usuarios', usuariosRouter); // Define a rota base para usuários
+
+app.use(express.static(path.join(__dirname, '..')));
+
+
 const PORT = 3000; //coloquei a 3000 mas quem quiser muda
 
 app.get('/', (req, res) => {
@@ -9,6 +20,5 @@ app.get('/', (req, res) => {
 });//testa a porra do serv
 
 app.listen(PORT, () => {
-     console.log(`Servidor rodando na porta ${PORT}`);
+     console.log(Servidor rodando na porta ${PORT});
 });//roda o serv
-
